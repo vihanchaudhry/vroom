6b32e56703024c78608296e4b150f4d9b38cbd14
Christopher Banh <chris@chrisbanh.com>
2016-05-07 16:36:37 -0700

    Ability to change mode

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarAIControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarAIControl.cs
@@ -64,7 +64,7 @@ namespace UnityStandardAssets.Vehicles.Car
             {
                 // Car should not be moving,
                 // use handbrake to stop
-                m_CarController.Move(0, 0, -1f, 1f);
+                m_CarController.Move(0, 0, -1f, 1f, 0);
             }
             else
             {
@@ -168,7 +168,7 @@ namespace UnityStandardAssets.Vehicles.Car
                 float steer = Mathf.Clamp(targetAngle*m_SteerSensitivity, -1, 1)*Mathf.Sign(m_CarController.CurrentSpeed);
 
                 // feed input to the car controller.
-                m_CarController.Move(steer, accel, accel, 0f);
+                m_CarController.Move(steer, accel, accel, 0f, 0);
 
                 // if appropriate, stop driving when we're close enough to the target.
                 if (m_StopWhenTargetReached && localTarget.magnitude < m_ReachTargetThreshold)
