f47874e32bcf4929343ea1217fee1efedc85cb54
Nir Ovadia <Ovadia.nir1@gmail.com>
2016-05-08 05:44:01 -0700

    speedometer done

---
+++ Assets/Speedometer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.car;
+
+public class Speedometer : MonoBehaviour {
+
+    private TextMesh tm;
+    private CarController cc;
+
+	// Use this for initialization
+	void Start () {
+        tm = GetComponent<TextMesh>();
+        cc = GetComponentInParent<CarController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tm.text = ((int)(cc.CurrentSpeed)).ToString();
+	}
+}
