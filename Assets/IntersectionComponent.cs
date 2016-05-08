63d59852badd6128642ebbf098c594b3784372b0
Nir Ovadia <Ovadia.nir1@gmail.com>
2016-05-08 00:51:24 -0700

    forgot a curly bracket

--- Assets/IntersectionComponent.cs
+++ Assets/IntersectionComponent.cs
@@ -1,30 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class IntersectionComponent : MonoBehaviour {
+public class IntersectionComponent : MonoBehaviour
+{
 
     private IntersectionReset dad;
     public float side = 1;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         dad = GetComponentInParent<IntersectionReset>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             if (dad.set(side) == 2)
             {
                 dad.calculate(other.GetComponent<PlayerProperties>());
             }
-            else (dad.setBlinker(other.GetComponent<PlayerProperties>().getBlinker());
+            else
+            {
+                dad.setBlinker(other.GetComponent<PlayerProperties>().getBlinker())
+                    }
         }
     }
 }
